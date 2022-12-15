4b5bfece7022caf4ad6b1877f00b624c856da50b
Dennis <alicorn0618@outlook.com>
2022-12-15 20:25:45 +0800

    finish some

--- official_document/Exercises/main.ml
+++ official_document/Exercises/main.ml
@@ -180,3 +180,145 @@ let decode list =
   | Many (n, x) :: t -> aux (many acc n x) t
 in
   aux [] (List.rev list)  
+  
+(* Run-length encoding of a list (direct solution) *)
+
+let encode list =
+  let rle count x = if count = 0 then One x else Many (count + 1, x) in
+  let rec aux count acc = function
+    | [] -> [] (* Can only be reached if original list is empty *)
+    | [x] -> rle count x :: acc
+    | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+                            else aux 0 (rle count a :: acc) t
+  in
+    List.rev (aux 0 [] list)
+
+(* Duplicate the elements of a list *)
+let duplicate lst =
+  let rec aux acc = function
+  | [] -> acc
+  | h :: t -> aux (h :: h :: acc) t
+in aux [] (List.rev lst)
+
+
+(* Replicate the elements of a list a given number of times *)
+
+let replicate lst n = 
+  let rec rep_1 ele acc n =
+    if n = 0 then acc
+    else rep_1  ele (ele :: acc) (n - 1)
+  in
+  let rec aux acc n =
+    function
+    | [] -> acc
+    | h :: t -> aux (rep_1 h acc n) n t 
+  in aux [] n lst
+
+(* Drop every N'th element from a list *)
+let drop lst n =
+  let rec aux acc cur_num n = function
+  | [] -> acc
+  | h :: t -> begin
+    if cur_num mod n = 0 
+      then aux acc (cur_num + 1) n t
+  else aux (h :: acc) (cur_num + 1) n t
+end
+in List.rev (aux [] 1 n lst)
+
+let drop list n =
+  let rec aux i = function
+  | [] -> []
+  | h :: t -> if i = n then aux 1 t else h :: aux (i + 1) t in
+  aux 1 list
+
+
+(* Split a list into two parts; the length of the first part is given *)
+let split lst n =
+  let rec aux acc cur_num n = function
+  | [] -> [List.rev acc, []]
+  | h :: t -> begin
+    if cur_num = n then
+      [List.rev (h::acc), t]
+    else
+      aux (h::acc) (cur_num + 1) n t
+    end
+  in 
+  assert (n > 0);
+  aux [] 1 n lst
+
+let split list n = 
+  let rec aux i acc = function
+    | [] -> List.rev acc, []
+    | h :: t as l -> if i = 0 then List.rev acc, l
+    else aux (i - 1) (h :: acc) t
+  in aux n [] list
+
+(* Extract a slice from a list *)
+let slice lst start finish = 
+  let lst_size = List.length lst in
+  assert (start < lst_size);
+  assert (finish < lst_size);
+  assert (start <= finish);
+  let rec aux acc pos start finish lst = 
+    if pos < start then
+      aux acc (pos + 1) start finish lst
+    else
+      if pos > finish then
+        acc
+      else
+        aux ((List.hd lst):: acc) (pos + 1) start finish (List.tl lst)
+      in aux [] 0 start finish lst
+
+let slice list i k =
+  let rec take n = function 
+  | [] -> []
+  | h :: t -> if n = 0 then [] else h :: take (n - 1) t
+in
+let rec drop n = function 
+  | [] -> []
+  | h :: t as l -> if n = 0 then l else drop (n - 1) t
+in take (k - i + 1) (drop i list)
+
+let rec fold_until f acc n = function
+  | [] -> (acc, [])
+  | h :: t as l -> if n = 0 then (acc, l)
+  else fold_until f (f acc h) (n - 1) t
+
+let slice list i k =
+  let _, list = fold_until (fun _ _ -> []) [] i list in
+  let taken, _ = fold_until (fun acc h -> h :: acc) [] (k - i + 1) list in
+  List.rev taken
+
+(* Rotate a list N places to the left *)
+
+let rotate lst n =
+    let first, second = split lst n in
+    List.rev (
+      List.append (List.rev first) (List.rev second)
+    )
+
+let split list n =
+  let rec aux i acc = function
+  | [] -> List.rev acc, []
+  | h :: t as l -> if i = 0 then List.rev acc, l
+  else aux (i - 1) (h :: acc) t in
+  aux n [] list
+
+let rotate list n =
+  let len = List.length list in
+  let n = if len = 0 then 0 else (n mod len + len) mod len in
+  if n = 0 then list
+  else let a, b = split list n in b @ a
+
+
+(* Remove the K'th element from a list *)
+
+let remove_at k lst = 
+  let rec aux acc k pos lst = 
+    if pos = k
+      then aux acc k (pos + 1) (List.tl lst)
+  else
+    match lst with
+    | [] -> acc
+    | h :: t -> aux (h :: acc) k (pos + 1) t
+  in List.rev (aux [] k 0 lst)
